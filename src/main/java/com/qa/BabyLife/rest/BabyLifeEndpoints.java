60c73b9b87bce16a87cf367046e58264f912cac2
alvinjo <alvin.joseph0@gmail.com>
2018-12-04 16:59:15 +0000

    asd

--- src/main/java/com/qa/BabyLife/rest/BabyLifeEndpoints.java
+++ src/main/java/com/qa/BabyLife/rest/BabyLifeEndpoints.java
@@ -2,10 +2,12 @@ package com.qa.BabyLife.rest;
 
 import com.qa.BabyLife.service.IBabyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RequestMapping("/lifespan")
 @RestController
 public class BabyLifeEndpoints {
